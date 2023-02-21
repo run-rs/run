8383f2074fca99f40a617e5e5b5f51102721d952
xionyuno123 <xionyuno@gmail.com>
2023-02-21 06:43:58 +0000

    update run tcp

--- run_tcp.sh
+++ run_tcp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FLAGS=$(getopt --long update, client,trace,period:,buf_size: -- "$@")
+FLAGS=$(getopt --long update, client,trace -- "$@")
 
 [ $? -eq 0 ] || {
   echo "Incorrect options provided"
@@ -12,21 +12,16 @@ eval set  "$FLAGS" --
 update=false
 client=false
 trace=false
-period=10
-buffer_size=9000
+
 
 while true; do
   case "$1" in
   --update ) update=true;shift;;
   --client ) client=true;shift;;
   --trace ) trace=true;shift;; 
-  --period ) shift; period=$1; shift;;
-  --buf_size ) shift; buffer_size=$1; shift;;
   -- ) break;;
   esac
 done
-
-echo "set period $period"
 
 if [ ! -d "install" ] || $update ; then
   echo "install examples ..."
@@ -39,20 +34,96 @@ fi
 
 
 # launch run_tcp
+echo "running pbuf...."
 if $client ; then
   if $trace ; then
-    sudo RUST_LOG=trace RUST_BACKTRACE=1 install/bin/run_tcp --client --period=$period --buf-size=$buffer_size
+    sudo RUST_LOG=trace RUST_BACKTRACE=1 install/bin/run_tcp --client --period=40 --buf-size=60000 --mtu=1518 > run_tcp.log 2>&1
   else
-    sudo install/bin/run_tcp --client --period=$period --buf-size=$buffer_size
+    sudo install/bin/run_tcp --client --period=30 --buf-size=60000 --mtu=1518 > run_tcp.log 2>&1
   fi
 else
   if $trace ; then
-    sudo RUST_LOG=trace RUST_BACKTRACE=1 install/bin/run_tcp --period=$period --buf-size=$buffer_size
+    sudo RUST_LOG=trace RUST_BACKTRACE=1 install/bin/run_tcp --period=40 --buf-size=60000 --mtu=1518 > run_tcp.log 2>&1
   else
-    sudo install/bin/run_tcp --period=$period --buf-size=$buffer_size
+    sudo install/bin/run_tcp --period=60 --buf-size=60000 --mtu=1518 > run_tcp.log 2>&1
   fi
 fi
+echo "done"
 
+
+echo "running cursor...."
+if $client ; then
+  if $trace ; then
+    sudo RUST_LOG=trace RUST_BACKTRACE=1 install/bin/run_tcp_cursor --client --period=40 --buf-size=60000 --mtu=1518 > run_tcp_cursor.log 2>&1
+  else
+    sudo install/bin/run_tcp_cursor --client --period=30 --buf-size=60000 --mtu=1518 > run_tcp_cursor.log 2>&1
+  fi
+else
+  if $trace ; then
+    sudo RUST_LOG=trace RUST_BACKTRACE=1 install/bin/run_tcp_cursor --period=40 --buf-size=60000 --mtu=1518 > run_tcp_cursor.log 2>&1
+  else
+    sudo install/bin/run_tcp_cursor --period=60 --buf-size=60000 --mtu=1518 > run_tcp_cursor.log 2>&1
+  fi
+fi
+echo "done"
+
+echo "running smoltcp...."
+if $client ; then
+  if $trace ; then
+    sudo RUST_LOG=trace RUST_BACKTRACE=1 install/bin/smol_tcp --client --period=40 --buf-size=60000 --mtu=1518 > smol_tcp.log 2>&1
+  else
+    sudo install/bin/smol_tcp --client --period=30 --buf-size=60000 --mtu=1518 > smol_tcp.log 2>&1
+  fi
+else
+  if $trace ; then
+    sudo RUST_LOG=trace RUST_BACKTRACE=1 install/bin/smol_tcp --period=40 --buf-size=60000 --mtu=1518 > smol_tcp.log 2>&1
+  else
+    sudo install/bin/smol_tcp --period=60 --buf-size=60000 --mtu=1518 > smol_tcp.log 2>&1
+  fi
+fi
+echo "done"
+
+
+echo "running pnet...."
+if $client ; then
+  if $trace ; then
+    sudo RUST_LOG=trace RUST_BACKTRACE=1 install/bin/pnet_tcp --client --period=40 --buf-size=60000 --mtu=1518 > pnet_tcp.log 2>&1
+  else
+    sudo install/bin/pnet_tcp --client --period=30 --buf-size=60000 --mtu=1518 > pnet_tcp.log 2>&1
+  fi
+else
+  if $trace ; then
+    sudo RUST_LOG=trace RUST_BACKTRACE=1 install/bin/pnet_tcp --period=40 --buf-size=60000 --mtu=1518 > pnet_tcp.log 2>&1
+  else
+    sudo install/bin/pnet_tcp --period=60 --buf-size=60000 --mtu=1518 > pnet_tcp.log 2>&1
+  fi
+fi
+echo "done"
+
+
+echo "running tso...."
+if $client ; then
+  if $trace ; then
+    #sudo RUST_LOG=trace RUST_BACKTRACE=1 install/bin/run_tso --client --period=40 --buf-size=60000 --mtu=1518 > run_tso.log 2>&1
+  else
+    #sudo install/bin/run_tso --client --period=30 --buf-size=60000 --mtu=1518 > run_tso.log 2>&1
+  fi
+else
+  if $trace ; then
+    #sudo RUST_LOG=trace RUST_BACKTRACE=1 install/bin/run_tso --period=40 --buf-size=60000 --mtu=1518 > run_tso.log 2>&1
+  else
+    #sudo install/bin/run_tso --period=60 --buf-size=60000 --mtu=1518 > run_tso.log 2>&1
+  fi
+fi
+echo "done"
+
+
+
+
+
+
+
+# plot figures
   
 
 
